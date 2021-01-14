048361ec1c11eff837971ca8808b942ca9fe41db
Avinash Kumar <avinashbest3@gmail.com>
2021-01-14 21:17:52 +0530

    Added Introduction to python directory

--- DataTypes.py
+++
@@ -1,7 +0,0 @@
-# Dyanamically typed
-# Not needed to upfront say the interpretor to allocate memory for int, float, char, string etc.
-a = 10
-print(a)
-# python will automatically create some storage and store the address of that variable every, every-time it store any another data_type
-a = "avinashbest"
-print(a)
--- Variables.py
+++
@@ -1,9 +0,0 @@
-# Variables
-a = 10
-b = 20
-
-sum = a + b
-abc = 10
-
-print(sum)
-print(abc)
