92f5c542a267ff7cc141c90eccbb63d0b0931d68
Avinash Kumar <avinashbest3@gmail.com>
2021-01-15 20:00:57 +0530

    Added Power Calculatation

---
+++ 01-IntroductionToPython/PowerCalculate.py
@@ -0,0 +1,9 @@
+# You are given two integers: X and N. You have to calculate X raised to power N and print it.
+
+# Write your code here
+X = int(input())
+N = int(input())
+
+power = (X ** N)
+
+print(power)
